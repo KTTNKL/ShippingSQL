2685be6a7e54ec66fc1fbe0269f16abe928d79f4
19127312 <19127312@student.hcmus.edu.vn>
2021-12-12 15:28:03 +0700

    Login NV/KH

--- Shipping/src/Login.java
+++ Shipping/src/Login.java
@@ -1,10 +1,11 @@
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class Login extends JFrame {
     private JTextField usernameT;
     private JTextField passwordT;
     private JButton loginButton;
-    private JButton registerButton;
     private JPanel loginPanel;
 
     public Login(){
@@ -13,7 +14,48 @@ public class Login extends JFrame {
         setSize(450,300);
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         setVisible(true);
+        loginButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if(passwordT.getText().equals("") || usernameT.getText().equals("")){
+                    JOptionPane.showMessageDialog(null,
+                            "Both field must not be blank",
+                            "Login failed",
+                            JOptionPane.WARNING_MESSAGE);
+                }else{
+                    DataConnection app = new DataConnection();
+                    if(passwordT.getText().equals("")){
+                        JOptionPane.showMessageDialog(null,
+                                "Username or Password is incorrect",
+                                "Login failed",
+                                JOptionPane.WARNING_MESSAGE);
+                    }else if(passwordT.getText().equals("123")){
+                        if(usernameT.getText().equals("login1")){
+                            new Profile();//KH
+                        }
+                        else if(usernameT.getText().equals("login3")){
+                            new ManageAccount();//NV
+
+                        }else if(usernameT.getText().equals("login6")){
+                            new ManageAccount();//NV
+
+                        }else{
+                            JOptionPane.showMessageDialog(null,
+                                    "Username or Password is incorrect",
+                                    "Login failed",
+                                    JOptionPane.WARNING_MESSAGE);
+                        }
+                    }else{
+                        JOptionPane.showMessageDialog(null,
+                                "Username or Password is incorrect",
+                                "Login failed",
+                                JOptionPane.WARNING_MESSAGE);
+                    }
+                }
+            }
+        });
     }
+
     public static void main(String[] args) {
         Login loginFrame = new Login();
     }
